59103a7681d0653b1277789ff57403dd3c7ff22a
lucasdtownsend <lucasdtownsend@gmail.com>
2018-09-19 13:22:26 -0400

    Initial Commit

---
+++ FizzBuzz1/FizzBuzzer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz1
+{
+    public class FizzBuzzer
+    {
+        public string Get(int input)
+        {
+            if (IsMultipleOf3(input) && IsMultipleOf5(input))
+            {
+                return "FizzBuzz";
+            }
+            if (IsMultipleOf3(input))
+            {
+                return "Fizz";
+            }
+            if (IsMultipleOf5(input))
+            {
+                return "Buzz";
+            }
+            return input.ToString();
+        }
+
+        private bool IsMultipleOf3(int input)
+        {
+            return input % 3 == 0;
+        }
+
+        private bool IsMultipleOf5(int input)
+        {
+            return input % 5 == 0;
+        }
+    }
+}
